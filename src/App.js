67c8a0c6ba55f37fa00bef33fe665f7388e9f8fe
Vamsi Krishna <92307727+invvamsikrishna@users.noreply.github.com>
2023-12-25 16:28:48 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import Router from "./routes";
 import ThemeProvider from "./theme";
 import SnackBar from "./components/SnackBar";
-import { ErrorBoundary } from "react-error-boundary";
+// import { ErrorBoundary } from "react-error-boundary";
 
-const FallbackComponent = ({ error, resetErrorBoundary }) => (
-  <div>
-    <p>Something went wrong:</p>
-    <pre>{error.message}</pre>
-    <button onClick={resetErrorBoundary}>Try again</button>
-  </div>
-);
+// const FallbackComponent = ({ error, resetErrorBoundary }) => (
+//   <div>
+//     <p>Something went wrong:</p>
+//     <pre>{error.message}</pre>
+//     <button onClick={resetErrorBoundary}>Try again</button>
+//   </div>
+// );
 
 function App() {
   return (
     <ThemeProvider>
       <SnackBar />
 
-      <ErrorBoundary FallbackComponent={FallbackComponent}>
-        <Router />
-      </ErrorBoundary>
+      {/* <ErrorBoundary FallbackComponent={FallbackComponent}> */}
+      <Router />
+      {/* </ErrorBoundary> */}
     </ThemeProvider>
   );
 }
