5f2ccdaab996fd2c34ecc3c2cf0a098ac1d42121
Vamsi Krishna <92307727+invvamsikrishna@users.noreply.github.com>
2024-01-11 18:53:25 +0530

    designation access

--- src/actions/other.js
+++ src/actions/other.js
@@ -7,13 +7,13 @@ export const clearOtherReducer = () => async (dispatch) => {
   });
 };
 
-export const getAllAccessPermissions = () => async (dispatch) => {
+export const getAllAccessPermissions = (id) => async (dispatch) => {
   dispatch({
     type: "OTHER_LOAD_START",
   });
 
   try {
-    const response = await ApiServices.postRequest(getPermissionListRoute);
+    const response = await ApiServices.postRequest(`${getPermissionListRoute}/${id}`);
     const responseData = response.data?.message ?? [];
 
     dispatch({
@@ -30,8 +30,15 @@ export const getAllAccessPermissions = () => async (dispatch) => {
 };
 
 export const updateAccessPermission = (name, value, jsonData) => async (dispatch) => {
+  // var data = { ...jsonData, [name]: value ? 1 : 0 };
+  // var updatedData = { [`${name}_${data.design_id}`]: value ? 1 : 0 };
+  // dispatch({
+  //   type: "OTHER_UPDATE_ACCESS",
+  //   payload: { data, updatedData },
+  // });
+
   var data = { ...jsonData, [name]: value ? 1 : 0 };
-  var updatedData = { [`${name}_${data.design_id}`]: value ? 1 : 0 };
+  var updatedData = { [`${name}`]: value ? 1 : 0 };
   dispatch({
     type: "OTHER_UPDATE_ACCESS",
     payload: { data, updatedData },
--- src/sections/reports/AccessMangementList.js
+++ src/sections/reports/AccessMangementList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import CustomMuiDataTable from "../../components/CustomMuiDataTable";
-import { Box, Card, Checkbox, CircularProgress, Divider, Stack, Typography } from "@mui/material";
+import { Box, Card, Checkbox, CircularProgress, Divider, Grid, MenuItem, Stack, TextField, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { connect } from "react-redux";
@@ -8,6 +8,7 @@ import { getAllAccessPermissions, clearOtherReducer, updateAccessPermission, sav
 import { LoadingButton } from "@mui/lab";
 import { ROWS_PER_PAGE_OPTION } from "../../constants";
 import { showAlert } from "../../actions/alert";
+import { UncontrolledTextField } from "../../components/hook-form/RHFTextField";
 
 const CustomCheckbox = styled(Checkbox)(({ theme }) => ({ padding: 0, "& .MuiSvgIcon-root": { fontSize: 17 } }));
 
@@ -30,9 +31,10 @@ const CustomCheckboxWithLabel = ({ label, ...props }) => (
 
 const AccessMangementList = ({ common, other, showAlert, getAllAccessPermissions, updateAccessPermission, clearOtherReducer }) => {
   const [loading, setLoading] = useState(false);
+  const [designation, setDesignation] = useState(common.designation[0]?.value || "");
 
   useEffect(() => {
-    getAllAccessPermissions();
+    handleSearch();
 
     return () => {
       clearOtherReducer();
@@ -48,49 +50,82 @@ const AccessMangementList = ({ common, other, showAlert, getAllAccessPermissions
       name: "page_name",
       label: "Page Name",
     },
-    ...common.designation.map((item) => {
-      var desPk = item.value;
+    // ...common.designation.map((item) => {
+    //   var desPk = item.value;
 
-      return {
-        name: `access_permissions_pk_${desPk}`,
-        label: item.label,
-        options: {
-          customBodyRender: (value, tableMeta, updateValue) => {
-            var pageId = tableMeta.rowData[0];
-            var rowIndex = other.data.findIndex((e) => e.page_id == pageId);
-            var accessData = other.data[rowIndex];
+    //   return {
+    //     name: `access_permissions_pk_${desPk}`,
+    //     label: item.label,
+    //     options: {
+    //       customBodyRender: (value, tableMeta, updateValue) => {
+    //         var pageId = tableMeta.rowData[0];
+    //         var rowIndex = other.data.findIndex((e) => e.page_id == pageId);
+    //         var accessData = other.data[rowIndex];
 
-            var data = {
-              page_id: pageId,
-              design_id: desPk,
-              access_permissions_pk: accessData[`access_permissions_pk_${desPk}`],
-              page_access: accessData[`page_access_${desPk}`],
-              add_perm: accessData[`add_perm_${desPk}`],
-              edit_perm: accessData[`edit_perm_${desPk}`],
-              view_perm: accessData[`view_perm_${desPk}`],
-              delete_perm: accessData[`delete_perm_${desPk}`],
-              approved_perm: accessData[`approved_perm_${desPk}`],
-            };
+    //         var data = {
+    //           page_id: pageId,
+    //           design_id: desPk,
+    //           access_permissions_pk: accessData[`access_permissions_pk_${desPk}`],
+    //           page_access: accessData[`page_access_${desPk}`],
+    //           add_perm: accessData[`add_perm_${desPk}`],
+    //           edit_perm: accessData[`edit_perm_${desPk}`],
+    //           view_perm: accessData[`view_perm_${desPk}`],
+    //           delete_perm: accessData[`delete_perm_${desPk}`],
+    //           approved_perm: accessData[`approved_perm_${desPk}`],
+    //         };
 
-            return (
-              <Box alignItems="center">
-                {/* <CustomCheckboxWithLabel label="Access Menu" labelPlacement="top" /> */}
+    //         return (
+    //           <Box alignItems="center">
+    //             {/* <CustomCheckboxWithLabel label="Access Menu" labelPlacement="top" /> */}
 
-                <Stack direction="row" spacing={1}>
-                  <CustomCheckboxWithLabel label="PA" checked={accessData[`page_access_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`page_access`, e.target.checked, data)} />
-                  <CustomCheckboxWithLabel label="C" checked={accessData[`add_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`add_perm`, e.target.checked, data)} />
-                  <CustomCheckboxWithLabel label="R" checked={accessData[`view_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`view_perm`, e.target.checked, data)} />
-                  <CustomCheckboxWithLabel label="U" checked={accessData[`edit_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`edit_perm`, e.target.checked, data)} />
-                  <CustomCheckboxWithLabel label="D" checked={accessData[`delete_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`delete_perm`, e.target.checked, data)} />
-                  <CustomCheckboxWithLabel label="AP" checked={accessData[`approved_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`approved_perm`, e.target.checked, data)} />
-                  <hr />
-                </Stack>
-              </Box>
-            );
-          },
+    //             <Stack direction="row" spacing={1}>
+    //               <CustomCheckboxWithLabel label="PA" checked={accessData[`page_access_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`page_access`, e.target.checked, data)} />
+    //               <CustomCheckboxWithLabel label="C" checked={accessData[`add_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`add_perm`, e.target.checked, data)} />
+    //               <CustomCheckboxWithLabel label="R" checked={accessData[`view_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`view_perm`, e.target.checked, data)} />
+    //               <CustomCheckboxWithLabel label="U" checked={accessData[`edit_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`edit_perm`, e.target.checked, data)} />
+    //               <CustomCheckboxWithLabel label="D" checked={accessData[`delete_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`delete_perm`, e.target.checked, data)} />
+    //               <CustomCheckboxWithLabel label="AP" checked={accessData[`approved_perm_${desPk}`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`approved_perm`, e.target.checked, data)} />
+    //               <hr />
+    //             </Stack>
+    //           </Box>
+    //         );
+    //       },
+    //     },
+    //   };
+    // }),
+
+    {
+      name: `access_permissions_pk`,
+      label: common.designation?.find((e) => e.value)?.label || "Designation",
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => {
+          var pageId = tableMeta.rowData[0];
+          var rowIndex = other.data.findIndex((e) => e.page_id == pageId);
+          var accessData = other.data[rowIndex];
+
+          var data = {
+            page_id: pageId,
+            design_id: designation,
+            ...accessData,
+          };
+
+          return (
+            <Box alignItems="center">
+              {/* <CustomCheckboxWithLabel label="Access Menu" labelPlacement="top" /> */}
+              <Stack direction="row" spacing={1}>
+                <CustomCheckboxWithLabel label="PA" checked={accessData[`page_access`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`page_access`, e.target.checked, data)} />
+                <CustomCheckboxWithLabel label="C" checked={accessData[`add_perm`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`add_perm`, e.target.checked, data)} />
+                <CustomCheckboxWithLabel label="R" checked={accessData[`view_perm`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`view_perm`, e.target.checked, data)} />
+                <CustomCheckboxWithLabel label="U" checked={accessData[`edit_perm`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`edit_perm`, e.target.checked, data)} />
+                <CustomCheckboxWithLabel label="D" checked={accessData[`delete_perm`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`delete_perm`, e.target.checked, data)} />
+                <CustomCheckboxWithLabel label="AP" checked={accessData[`approved_perm`] == 1 ? true : false} onChange={(e) => updateAccessPermission(`approved_perm`, e.target.checked, data)} />
+                {/* <hr /> */}
+              </Stack>
+            </Box>
+          );
         },
-      };
-    }),
+      },
+    },
   ];
 
   const options = {
@@ -101,8 +136,18 @@ const AccessMangementList = ({ common, other, showAlert, getAllAccessPermissions
     rowsPerPageOptions: ROWS_PER_PAGE_OPTION,
     download: false,
     print: false,
-    viewColumns: true,
+    viewColumns: false,
     filter: false,
+  };
+
+  const handleSearch = async () => {
+    if (!designation) {
+      return;
+    }
+
+    setLoading(true);
+    await getAllAccessPermissions(designation);
+    setLoading(false);
   };
 
   const handleSavePermissions = async () => {
@@ -110,7 +155,8 @@ const AccessMangementList = ({ common, other, showAlert, getAllAccessPermissions
     const response = await saveAccessPermission(other.changes);
     if (response == true) {
       showAlert({ text: "Permissions updated successfully", color: "success" });
-      getAllAccessPermissions();
+
+      handleSearch();
     } else {
       showAlert({ text: "Something went wrong" });
     }
@@ -119,6 +165,38 @@ const AccessMangementList = ({ common, other, showAlert, getAllAccessPermissions
 
   return (
     <Card>
+      <Box p={3}>
+        <Typography variant="body1">Assign Authority</Typography>
+
+        <Box p={1} />
+
+        <Grid container spacing={2} sx={{ alignItems: "center" }}>
+          <Grid item xs={3}>
+            <UncontrolledTextField label="Select designation" value={designation} onChange={(e) => setDesignation(e.target.value)} select>
+              {common.designation.map((item, index) => (
+                <MenuItem key={index} value={item.value}>
+                  {item.label}
+                </MenuItem>
+              ))}
+            </UncontrolledTextField>
+          </Grid>
+
+          <Grid item xs={3}>
+            <LoadingButton loading={loading} variant="contained" onClick={handleSearch}>
+              Search
+            </LoadingButton>
+          </Grid>
+
+          <Grid item xs={6} sx={{ textAlign: "end" }}>
+            <LoadingButton loading={loading} variant="contained" onClick={handleSavePermissions}>
+              Save
+            </LoadingButton>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Divider />
+
       {other.isLoading && (
         <Box minHeight={200} display="flex" justifyContent="center" alignItems="center">
           <CircularProgress />
@@ -127,16 +205,6 @@ const AccessMangementList = ({ common, other, showAlert, getAllAccessPermissions
 
       {!other.isLoading && (
         <>
-          <Stack direction="row" p={3} sx={{ alignItems: "center", justifyContent: "space-between" }}>
-            <Typography variant="body1">Assign Authority</Typography>
-
-            <LoadingButton loading={loading} variant="contained" onClick={handleSavePermissions}>
-              Save
-            </LoadingButton>
-          </Stack>
-
-          <Divider />
-
           <CustomMuiDataTable title="" columns={columns} data={other.data} options={options} />
         </>
       )}
